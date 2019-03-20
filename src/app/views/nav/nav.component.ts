e02299489255db2252a57ed4913b983c117e0fd7
cheskyposen <shragaposen@gmail.com>
2019-03-20 16:08:10 -0400

    hello to more

--- src/app/views/nav/nav.component.ts
+++ src/app/views/nav/nav.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
@@ -8,8 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   title = 'tvmaze search';
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit() {}
 
+  goBack(): void {
+    this.location.back();
+  }
 }
